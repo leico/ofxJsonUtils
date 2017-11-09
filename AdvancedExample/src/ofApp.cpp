b56914996ab59a5f9ea208b83d6517eae8dd7baf
ISHII 2bit <ishii@buffer-renaiss.com>
2017-11-09 17:07:27 +0900

    update example

--- AdvancedExample/src/ofApp.cpp
+++ AdvancedExample/src/ofApp.cpp
@@ -8,16 +8,14 @@ struct custom_struct {
     float other_value;
 };
 
-// 1.2 define convert and parse function for custom struct
+// 1.2 define to_json and from_json function for custom struct on SAME NAMESPACE
 
-ofJson convert(const custom_struct &ps) {
-    ofJson json;
+void to_json(ofJson &json, const custom_struct &ps) {
     json["value"] = ps.value;
     json["other_value"] = ps.other_value;
-    return json;
 }
 
-void parse(const ofJson &json, custom_struct &ps) {
+void from_json(const ofJson &json, custom_struct &ps) {
     ps.value = json["value"];
     ps.other_value = json["other_value"];
 }
@@ -41,18 +39,20 @@ struct other_custom_struct {
 };
 
 // 3.1 define nested struct
-// this can use ofxJsonUtils::create, ofxJsonUtils::load
+// this can use ofxJsonUtils::create, ofxJsonUtils::load, or OFX_JSONIZE
 
 struct nested_custom_struct : ofxJsonify<nested_custom_struct> {
     ofVec2f v;
     custom_struct cs;
     other_custom_struct ocs;
-    ofJson toJson() const {
-        return ofxJsonUtils::create(kv(v), kv(cs), kv(ocs));
-    }
-    void loadJson(const ofJson &json) {
-        ofxJsonUtils::load(json, kv(v), kv(cs), kv(ocs));
-    }
+    OFX_JSONIZE(nested_custom_struct, v, cs, ocs);
+//    is same as:
+//    ofJson toJson() const {
+//        return ofxJsonUtils::create(kv(v), kv(cs), kv(ocs));
+//    }
+//    void loadJson(const ofJson &json) {
+//        ofxJsonUtils::load(json, kv(v), kv(cs), kv(ocs));
+//    }
 };
 
 // 4.1 ofxJsonify
@@ -78,15 +78,33 @@ public:
         
         {
             std::vector<int> xs{1, 2, 3, 4, 5};
-            ofJson json = ofxJsonUtils::convert(xs);
+            ofJson json = xs;
             ofLogNotice("vector<int>") << json.dump();
         }
         
         {
             std::vector<ofVec3f> vs;
             for(std::size_t i = 0; i < 4; i++) vs.emplace_back(i, i * i, i * i * i);
-            ofJson json = ofxJsonUtils::convert(vs);
+            ofJson json = vs;
             ofLogNotice("vector<ofVec3f> dump") << json.dump();
+        }
+        
+        {
+            custom_struct cs;
+            
+            cs.value = 42.0f;
+            cs.other_value = 9999.9f;
+            // if to_json is imeplemented,
+            // then it can implicit convert to ofJson
+            ofSaveJson("custom_struct.json", cs);
+            
+            custom_struct cs2;
+            
+            // if from_json is imeplemented,
+            // then it can implicit convert to ofJson
+            cs2 = ofLoadJson("custom_struct.json");
+            
+            ofLogNotice("cs2") << cs2.value << ", " << cs2.other_value;
         }
         
         {
@@ -98,12 +116,18 @@ public:
             });
             p.loadJson(json);
             ofLogNotice("other_custom_struct: after load") << p.toJson().dump();
+            other_custom_struct q = json;
+            q.value += 1.0f;
+            json = q;
+            ofLogNotice("json: ") << json.dump();
         }
         
         {
             jsonify_test jt;
             jt.x = 26;
             ofLogNotice("jsonify_test: jt") << jt.toJson().dump();
+            // if inherit ofxJsonify<Class>,
+            // then implement loadFromJsonFile and writeToJsonFile automatically
             jt.writeToJsonFile("test.json");
             jsonify_test jt2;
             jt2.loadFromJsonFile("test.json");
